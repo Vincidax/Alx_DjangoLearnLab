8c7ceec670ad4ca2e4f45350e649d668ac51c6d0
vincidax <vincentdx18@gmail.com>
2025-11-16 17:01:59 +0200

    fbv and cbv

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
+
+     path('books/', views.list_books, name='list_books'),  # FBV
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # CBV
+
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
+from django.views.generic import DetailView
+from .models import Book, Library
+
+# ---------------------------
+# Function-based view to list all books
+# ---------------------------
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# ---------------------------
+# Class-based view for library detail
+# ---------------------------
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # the object will be available as 'library' in template
+
 
 # ---------------------------
 # User Login
