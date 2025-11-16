9cba39ec00894e7f65ccff341465087cade57d9b
vincidax <vincentdx18@gmail.com>
2025-11-16 17:09:05 +0200

    fbv and cbv

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 # relationship_app/views.py
 
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
