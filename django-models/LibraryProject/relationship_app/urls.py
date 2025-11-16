39a4891702550720ebde75d0b031984827e34cea
vincidax <vincentdx18@gmail.com>
2025-11-16 17:23:51 +0200

    updating views.py usercreationform;

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,18 +1,27 @@
 # relationship_app/urls.py
 
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from .views import user_login, user_logout, register, LibraryDetailView
 
 urlpatterns = [
-    # Authentication URLs
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
-    path('register/', register, name='register'),
+    # Authentication
+    path(
+        "login/",
+        LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login"
+    ),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout"
+    ),
+    path("register/", register, name="register"),
 
-    # Function-Based View (FBV)
-    path('books/', list_books, name='list_books'),
+    # Function-based view
+    path("books/", list_books, name="list_books"),
 
-    # Class-Based View (CBV)
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # Class-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
 ]
