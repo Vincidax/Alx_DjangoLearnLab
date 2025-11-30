d628e23c8b2c3e6c6670f858e2e1a8577ddec714
vincidax <vincentdx18@gmail.com>
2025-11-30 13:00:34 +0200

    add documentation and authentication 2

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', BookListView.as_view(), name='books-list'),            # List all books
     path('books/create/', BookCreateView.as_view(), name='books-create'), # Create a book
     path('books/<int:pk>/', BookDetailView.as_view(), name='books-detail'),  # Retrieve a single book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='books-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='books-delete'),  # Delete a book
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='books-update'),  # Update a book
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='books-delete'),  # Delete a book
 ]
