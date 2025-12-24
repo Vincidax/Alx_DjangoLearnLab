54f801fac7d20459ccbc99678ce5d3547cb084a5
vincidax <vincentdx18@gmail.com>
2025-12-24 13:52:07 +0200

    adding views, templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm
 
-# Create your views here.
+def home(request):
+    return render(request, 'blog/home.html')
+
+def posts(request):
+    return render(request, 'blog/posts.html') 
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+        return redirect('profile')
+    return render(request, 'blog/profile.html')
