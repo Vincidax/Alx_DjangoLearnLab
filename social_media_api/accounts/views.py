8a70fd2a2d16b9d346bb4038c2e88be66cbf4648
vincidax <vincentdx18@gmail.com>
2025-12-24 17:26:04 +0200

    adding models

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
@@ -41,3 +43,26 @@ class ProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+        request.user.following.add(user_to_follow)
+        return Response({'message': 'User followed successfully'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'message': 'User unfollowed successfully'})
+    except User.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
